5da0ba5716697b354560fd63e8dac4dff4bcf788
leesmarths1 <43789944+leesmarths1@users.noreply.github.com>
2019-02-14 14:46:46 +0000

    dsgd
    sdfg

--- setenv.sh
+++ setenv.sh
@@ -1,1 +1,1 @@
-JAVA_OPTS=" -Duser.home=/usr/local/ -Dpega.tmpdir=/usr/data/ -Dindex.directory=/usr/data/  -Dpega.logdir=/usr/data/"
+JAVA_OPTS=" -Xms8192m -Xmx16384m -Duser.home=/usr/local/ -Dpega.tmpdir=/usr/data/ -Dindex.directory=/usr/data/  -Dpega.logdir=/usr/data/"
